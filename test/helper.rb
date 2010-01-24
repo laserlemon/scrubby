1c3c5179ae347432eec2721d8f6c6bc174c18256
laserlemon <steve.richert@gmail.com>
2010-01-24 00:19:30 -0500

    Added schema information to the test helper.

--- test/helper.rb
+++ test/helper.rb
@@ -1,10 +1,36 @@
+$: << File.join(File.dirname(__FILE__), '..', 'lib')
+$: << File.dirname(__FILE__)
+
 require 'rubygems'
 require 'test/unit'
+require 'active_record'
 require 'shoulda'
+require 'mocha'
+require 'scrubby'
+begin; require 'redgreen'; rescue LoadError; end
 
-$LOAD_PATH.unshift(File.join(File.dirname(__FILE__), '..', 'lib'))
-$LOAD_PATH.unshift(File.dirname(__FILE__))
-require 'shoulda'
+ActiveRecord::Base.establish_connection(
+  :adapter => 'sqlite3',
+  :database => File.join(File.dirname(__FILE__), 'test.db')
+)
 
-class Test::Unit::TestCase
+class CreateSchema < ActiveRecord::Migration
+  def self.up
+    create_table :users, :force => true do |t|
+      t.string :type
+      t.string :first_name
+      t.string :last_name
+      t.timestamps
+    end
+  end
+end
+
+CreateSchema.suppress_messages do
+  CreateSchema.migrate(:up)
+end
+
+class User < ActiveRecord::Base
+end
+
+class Admin < User
 end
